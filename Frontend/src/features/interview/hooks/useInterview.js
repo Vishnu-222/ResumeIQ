1e893f5443721e027669f1074dc6b1d8e11551b0
Vishnu Singh <vs414447@gmail.com>
2026-08-02 01:27:01 +0530

    Added Resume Generation Feature.

--- Frontend/src/features/interview/hooks/useInterview.js
+++ Frontend/src/features/interview/hooks/useInterview.js
@@ -1,4 +1,4 @@
-import { generateInterviewReport, getInterviewReportById , getAllInterviewReports } from "../services/interview.api"
+import { generateInterviewReport, getInterviewReportById , getAllInterviewReports , generateResumePdf } from "../services/interview.api"
 import { useContext, useEffect } from "react"
 import { InterviewContext } from "../interview.context"
 import { useParams } from "react-router"
@@ -58,6 +58,25 @@ export const useInterview = () => {
         return response.interviewReports
     }
 
+    const getResumePdf = async (interviewReportId) => {
+        setLoading(true)
+        let response = null
+        try {
+            response = await generateResumePdf({ interviewReportId })
+            const url = window.URL.createObjectURL(new Blob([ response ], { type: "application/pdf" }))
+            const link = document.createElement("a")
+            link.href = url
+            link.setAttribute("download", `resume_${interviewReportId}.pdf`)
+            document.body.appendChild(link)
+            link.click()
+        }
+        catch (error) {
+            console.log(error)
+        } finally {
+            setLoading(false)
+        }
+    }
+
     useEffect(() => {
         if (interviewId) {
             getReportById(interviewId)
@@ -66,6 +85,6 @@ export const useInterview = () => {
         }
     }, [ interviewId ])
 
-    return { loading, report, reports, generateReport, getReportById, getReports }
+    return { loading, report, reports, generateReport, getReportById, getReports , getResumePdf }
 
 }
