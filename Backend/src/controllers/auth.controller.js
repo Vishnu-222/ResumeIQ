8dd0882e8ed5ce39c3c395ad066834a8e2308d67
Vishnu Singh <vs414447@gmail.com>
2026-08-07 18:15:31 +0530

    Login cookie configuration modified.

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -92,7 +92,12 @@ async function loginUserController(req, res , next) {
             { expiresIn: "1d" }
         )
         
-        res.cookie("token", token)
+        res.cookie("token", token, {
+            httpOnly: true,
+            secure: false,
+            sameSite: "lax",
+            maxAge: 24 * 60 * 60 * 1000,
+        });
         
         return res.status(200).json({
             success: true,
